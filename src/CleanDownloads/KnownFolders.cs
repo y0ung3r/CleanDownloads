3c4e657a464bbe7b7bcda1b68052878bb8cc85c9
Nikita Velikiy <mtyoungstz@outlook.com>
2025-11-09 20:18:58 +0500

    Return an implementation that gets the path to the Downloads folder via the user account.

--- src/CleanDownloads/KnownFolders.cs
+++ src/CleanDownloads/KnownFolders.cs
@@ -1,92 +1,38 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
-using Microsoft.Win32;
+using System.Runtime.InteropServices;
 
 namespace CleanDownloads;
 
 public static class KnownFolders
 {
-    private static readonly string[] SystemProfiles =
-    [
-        // Default
-        ".DEFAULT", 
-        
-        // Local system
-        "S-1-5-18", 
-        
-        // Local service
-        "S-1-5-19", 
-        
-        // Network service
-        "S-1-5-20"
-    ];
-    
-    private const string ProfileListRegistryKey
-        = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList";
-    
-    private const string ProfileImagePathVariable
-        = "ProfileImagePath";
-    
-    private const string UserShellFoldersRegistryKeyTemplate
-        = @"{0}\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders";
-    
-    private const string UserProfileVariable
-        = "%USERPROFILE%";
-    
-    private static string ResolveUserProfilePath(RegistryKey profile)
-    {
-        var profileImagePathVariable = profile.GetValue(ProfileImagePathVariable);
-        
-        if (profileImagePathVariable is not string profileFolderPath)
-            throw new InvalidOperationException("Unable to find user profile path");
-        
-        if (!Directory.Exists(profileFolderPath))
-            throw new InvalidOperationException("User profile path is not exists");
-
-        return Environment.ExpandEnvironmentVariables(profileFolderPath); // Expand variables such as %SystemDrive%
-    }
-
     public static class Downloads
     {
-        private const string DownloadsFolderVariable
-            = "{374DE290-123F-4565-9164-39C4925E467B}";
+        private static readonly Guid DownloadsFolderVariable
+            = new("374DE290-123F-4565-9164-39C4925E467B");
         
         public static string Path
         {
             get
             {
-                using var profileList = Registry.LocalMachine.OpenSubKey(ProfileListRegistryKey) 
-                    ?? throw new InvalidOperationException("Unable to find Profile List registry key");
-                
-                var profileName = profileList
-                    .GetSubKeyNames()
-                    .FirstOrDefault(registryKey => !SystemProfiles.Contains(registryKey));
-                
-                if (profileName is null)
-                    throw new InvalidOperationException("Unable to find user profile");
-                
-                using var latestUser = profileList.OpenSubKey(profileName) 
-                    ?? throw new InvalidOperationException("Unable to find user profile");
-                
-                var profilePath = ResolveUserProfilePath(latestUser);
-                
-                using var userShellFolders = Registry.Users.OpenSubKey(string.Format(UserShellFoldersRegistryKeyTemplate, profileName))
-                    ?? throw new InvalidOperationException("Unable to find User Shell Folders for current user");
+                var searchResult = SHGetKnownFolderPath(DownloadsFolderVariable, dwFlags: 0, hToken: IntPtr.Zero, out var folderPath);
+            
+                if (searchResult is not 0) 
+                    Marshal.ThrowExceptionForHR(searchResult);
 
-                var downloadsFolderVariable = userShellFolders.GetValue(DownloadsFolderVariable, defaultValue: null, RegistryValueOptions.DoNotExpandEnvironmentNames);
-                
-                if (downloadsFolderVariable is not string downloadsFolderPath)
-                    throw new InvalidOperationException("Unable to find Downloads folder variable for current user");
-                
-                var expandedDownloadsPath = Environment.ExpandEnvironmentVariables(
-                    downloadsFolderPath.Replace(UserProfileVariable, profilePath, StringComparison.OrdinalIgnoreCase)); // Expand other variables if needed
-
-                if (!Directory.Exists(expandedDownloadsPath))
-                    throw new InvalidOperationException($"Downloads path \"{expandedDownloadsPath}\" is not exists");
-                
-                return expandedDownloadsPath;
+                try
+                {
+                    return Marshal.PtrToStringUni(folderPath) 
+                        ?? throw new InvalidOperationException("Unable to determine Downloads folder path");
+                }
+                finally
+                {
+                    if (folderPath != IntPtr.Zero)
+                        Marshal.FreeCoTaskMem(folderPath);
+                }
             }
         }
     }
+    
+    [DllImport("shell32.dll")]
+    private static extern int SHGetKnownFolderPath([MarshalAs(UnmanagedType.LPStruct)] Guid rfid, uint dwFlags, IntPtr hToken, out IntPtr ppszPath);
 }
