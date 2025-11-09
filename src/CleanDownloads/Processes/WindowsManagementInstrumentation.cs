10f600ce4b893e659e6d500f78af183099bb9d2d
Nikita Velikiy <mtyoungstz@outlook.com>
2025-11-09 08:37:29 +0500

    Prettify WMI consts

---
+++ src/CleanDownloads/Processes/WindowsManagementInstrumentation.cs
@@ -0,0 +1,19 @@
+﻿namespace CleanDownloads.Processes;
+
+public static class WindowsManagementInstrumentation
+{
+    public static class Processes
+    {
+        public const string TargetInstance = nameof(TargetInstance);
+        
+        public const string ProcessId = nameof(ProcessId);
+        
+        public const string ProcessName = "Name";
+        
+        public const string CommandLine = nameof(CommandLine);
+        
+        public const string SelectLaunched = "select * from __InstanceCreationEvent within 1 where TargetInstance isa 'Win32_Process'";
+        
+        public const string SelectTerminated = "select * from __InstanceDeletionEvent within 1 where TargetInstance isa 'Win32_Process'";
+    }
+}
