7ea0688ebc8f7320f72e56c72f07d2ae394ae330
Nikita Velikiy <mtyoungstz@outlook.com>
2025-11-09 08:37:07 +0500

    Configure logs for EventLog

--- src/CleanDownloads/Program.cs
+++ src/CleanDownloads/Program.cs
@@ -6,6 +6,19 @@ using CleanDownloads;
 
 var builder = Host.CreateApplicationBuilder(args);
 
+builder.Logging
+    .ClearProviders()
+    .SetMinimumLevel(LogLevel.Information)
+    .AddFilter<EventLogLoggerProvider>("Microsoft.Hosting.Lifetime", LogLevel.Warning)
+    .AddFilter<EventLogLoggerProvider>(string.Empty, LogLevel.Information)
+    .AddConsole()
+    .AddEventLog(new EventLogSettings
+    {
+        SourceName = "Clean Downloads",
+        LogName = "Application",
+        Filter = (_, logLevel) => logLevel >= LogLevel.Information
+    });
+
 builder.Services
     .AddHostedService<DownloadsWatcher>()
     .AddWindowsService(options => options.ServiceName = "Clean Downloads");
