1c67e2b15967ede30b9c181ab11f52f6979e65f1
Nikita Velikiy <mtyoungstz@outlook.com>
2025-11-09 20:25:20 +0500

    Migrate from Windows service to regular application

--- src/CleanDownloads/Program.cs
+++ src/CleanDownloads/Program.cs
@@ -3,27 +3,16 @@ using Microsoft.Extensions.Hosting;
 using CleanDownloads;
 using CleanDownloads.Processes;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.EventLog;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Logging
     .ClearProviders()
-    .SetMinimumLevel(LogLevel.Information)
-    .AddFilter<EventLogLoggerProvider>("Microsoft.Hosting.Lifetime", LogLevel.Warning)
-    .AddFilter<EventLogLoggerProvider>(string.Empty, LogLevel.Information)
-    .AddConsole()
-    .AddEventLog(new EventLogSettings
-    {
-        SourceName = "Clean Downloads",
-        LogName = "Application",
-        Filter = (_, logLevel) => logLevel >= LogLevel.Information
-    });
+    .AddConsole();
 
 builder.Services
     .AddSingleton<ProcessMonitor>()
-    .AddHostedService<WindowsService>()
-    .AddWindowsService(options => options.ServiceName = "Clean Downloads");
+    .AddHostedService<FileRecycler>();
 
 var host = builder.Build();
 
